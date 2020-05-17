1d95160cbc4220c1339e953fa9942c061e787fef
fisnikshatri <fs39047@ubt-uni.net>
2020-05-17 14:59:23 +0200

    Validimi i JS

--- Projekti/NightClub_Fisnik_Shatri/registration/script.js
+++ Projekti/NightClub_Fisnik_Shatri/registration/script.js
@@ -1,1 +1,25 @@
-const
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value == null){
+        messages.push('Username duhet te shkruhet')
+    }
+
+    if (password.value.length <= 6){
+        messages.push('Passwordi duhet te jete me i madh se 6 karaktere')
+    }
+
+    if (password.value.length >= 20){
+        messages.push('Passwordi duhet te jete me i shkurte se 20 karaktere')
+    }
+
+    if (messages.length > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+    
+})
